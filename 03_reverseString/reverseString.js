92923bb643027934837687aebd9c257227dbd3a8
missmioh <186388075+missmioh@users.noreply.github.com>
2024-12-11 15:50:58 +0100

    Complete task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let text = string.split('');
+    text.reverse();
+    return text.join('');
 };
 
 // Do not edit below this line
