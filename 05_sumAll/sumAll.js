983cf29cfbc703fd5c7826d21d4d0f4c1df61b2c
missmioh <186388075+missmioh@users.noreply.github.com>
2024-12-11 20:42:44 +0100

    Add function to add numbers within the range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = x;
 
+    for (let i = (x+1) ; i <= y; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
