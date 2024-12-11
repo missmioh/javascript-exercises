af71ead8e16d2cf613a7f21afd052ee1f840481e
missmioh <186388075+missmioh@users.noreply.github.com>
2024-12-11 15:51:40 +0100

    Complete task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if ( num < 0 ) {
+        return "ERROR";
+    }
+
+    let repeatOutcome = "";
+    for (let i = 0; i < num; i++) {
+        repeatOutcome += string;
+    }
+    return repeatOutcome;
 };
 
 // Do not edit below this line
