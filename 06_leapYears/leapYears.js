9e8b2a5fc6802c61787dbfc44ce3c1cf34a1eda1
missmioh <186388075+missmioh@users.noreply.github.com>
2024-12-15 17:11:56 +0100

    Add conditions for a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result;
+    if ( (year % 100 === 0) && (year % 400 === 0) ) {
+        return result = true;
+    } else if ( (year % 4 === 0) && (year % 100 === 0) ) {
+        return result = false;
+    } else if ( year % 4 === 0 ) {
+        return result = true;
+    } else {
+        return result = false;
+    }
 };
 
 // Do not edit below this line
