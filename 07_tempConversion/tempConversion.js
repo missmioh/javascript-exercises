686b59d2a5e36709fc264726350dee55d97b5235
missmioh <186388075+missmioh@users.noreply.github.com>
2024-12-15 19:07:44 +0100

    Add conversion formulae and method for rounding

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let convertedNumber = (num-32) * (5 / 9);
+  let roundedNumber = Math.round(convertedNumber * 10) / 10;
+  return roundedNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let convertedNumber = num*(9 / 5) + 32;
+  let roundedNumber = Math.round(convertedNumber * 10) / 10;
+  return roundedNumber;
 };
 
 // Do not edit below this line
