2f1c8406cc621e09034a8b2b9a11acae15eb5922
missmioh <186388075+missmioh@users.noreply.github.com>
2024-12-15 16:12:32 +0100

    Return ERROR message for various scenarios
    Function now returns error for NaN, non-integer and negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,17 @@
 const sumAll = function(x, y) {
     let sum = x;
 
-    for (let i = (x+1) ; i <= y; i++) {
-        sum += i;
+    if (x < 0 || y < 0) {
+        return "ERROR";
+    } else if (parseInt(x) !== x || parseInt(y) !== y) {
+        return "ERROR";
+    } else {
+        for (let i = (x+1) ; i <= y; i++) {
+           sum += i;
+        }
     }
     return sum;
-};
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
