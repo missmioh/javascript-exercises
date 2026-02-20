b1a51872e4f8c1fae49ca8ddcd3a62c7e57833fb
missmioh <186388075+missmioh@users.noreply.github.com>
2026-02-20 18:07:37 +0100

    Complete task without regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+// solution without regex
 
+const palindromes = function (string) {
+    let stringA = string.toLowerCase();
+
+    while (stringA.includes(" ") === true || stringA.includes(".") === true || stringA.includes("!") === true ||
+stringA.includes(",") === true) {
+    stringA = stringA.split(" ").join("");
+    stringA = stringA.split(",").join("");
+    stringA = stringA.split(".").join("");
+    stringA = stringA.split("!").join("");
+    stringA = stringA.split("").reverse().join("");
+    }
+
+    let stringB = stringA.split("").reverse().join("");
+    console.log(stringB);
+
+    if (stringA === stringB) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
