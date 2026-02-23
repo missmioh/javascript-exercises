44e1385adb9320b4a9fa548cb4e150bfbde1c80d
missmioh <186388075+missmioh@users.noreply.github.com>
2026-02-23 15:18:09 +0100

    Complete exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titleList = arr.map(book => book.title);
+    return titleList;
 };
 
 // Do not edit below this line
