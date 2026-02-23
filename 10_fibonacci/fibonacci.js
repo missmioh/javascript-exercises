3a9c42c58138ed4769f675a98aafd1f8fc89fa6a
missmioh <186388075+missmioh@users.noreply.github.com>
2026-02-23 14:49:56 +0100

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibArray = [1, 1];
+    let i = 0;
 
+    let number
+    if (typeof num !== 'number') {
+        number = parseInt(num);
+    } else {
+        number = num;
+    }
+
+    if (number === 0) return 0;
+    if (number < 0) return "OOPS";
+
+    while (fibArray.length < number) {
+        let sum = fibArray[i] + fibArray[++i];
+        fibArray.push(sum);
+    }
+
+    return fibArray[`${number - 1}`];
 };
 
 // Do not edit below this line
