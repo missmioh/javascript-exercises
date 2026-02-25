e97ea0fefb339b57e57e1e50d63f04a002f136a4
missmioh <186388075+missmioh@users.noreply.github.com>
2026-02-25 14:12:15 +0100

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const getAge = person =>
+        (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
 
-};
+        let oldest = arr.sort((a, b) => getAge(b) - getAge(a));
+
+        // original idea without missing dates
+
+        // const oldest = arr.sort((a, b) => {
+        // const personA = a.yearOfDeath - a.yearOfBirth;
+        // const personB = b.yearOfDeath - b.yearOfBirth;
+        // return personA > personB ? -1 : 1;
+        // }
+        return oldest[0];
+
+    };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
